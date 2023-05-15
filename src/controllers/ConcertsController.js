00e4a013cb028690fa4fb25d362df0909a5e9ee4
milosilic5 <ilicmilosmiki@gmail.com>
2023-05-15 21:39:17 +0200

    v5

--- src/controllers/ConcertsController.js
+++ src/controllers/ConcertsController.js
@@ -5,5 +5,5 @@ const ConcertsService = require("../services/ConcertsService");
 exports.index = async(req,res)=>{
     var concerts = await ConcertsService.findAllConcerts();
     //res.json(concerts);
-    res.render("concerts/main",{layout:"layouts/main",data:concerts})
+    res.render("concertParts/table",{layout:"layouts/main",data:concerts})
 }
--- src/controllers/DiscographyController.js
+++ src/controllers/DiscographyController.js
@@ -4,5 +4,5 @@ const DiscographyService = require("../services/DiscographyService");
 exports.index = async(req,res)=>{
     var allSongs = await DiscographyService.findAllSongs();
     //res.json(allSongs);
-    res.render("discography/main",{layout:"layouts/main", data: allSongs})
+    res.render("discographyParts/main",{layout:"layouts/main", data: allSongs})
 }
