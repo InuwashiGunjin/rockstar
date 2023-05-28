09faa601ada7205788d1d3567c6b27c1916e4ca9
milosilic5 <ilicmilosmiki@gmail.com>
2023-05-28 21:33:27 +0200

    final

--- src/controllers/ConcertsController.js
+++ src/controllers/ConcertsController.js
@@ -12,21 +12,20 @@ exports.index = async(req,res)=>{
 exports.reserveCard = async(req,res) => {
     var concertId=req.params.id_concert;
     var user = index.getUser(req.cookies["session"]);
-        if(typeof(user)=='undefined')
+        if(user==undefined)
             res.redirect("/auth/login")
-        else
-            {
-            var nonReservedCard = await ConcertsService.findOneNonReservedCard(concertId);
-            nonReservedCard=nonReservedCard[0].cards[0];
-        if(typeof(nonReservedCard)!='undefined')
+    else
+        {
+        var nonReservedCard = await ConcertsService.findOneNonReservedCard(concertId);
+        nonReservedCard=nonReservedCard[0].cards[0];
+        if(nonReservedCard!=undefined)
         {
             nonReservedCard.is_reserved = true;
             nonReservedCard.id_user_reserved = user.id_users;
             nonReservedCard =await CardService.reserveCard(nonReservedCard);
             res.redirect("/concerts/usersCards");
         }
-    else
-        {
+    else{
             res.redirect("/concerts");
         }
     }
@@ -36,21 +35,21 @@ exports.buyCard = async(req,res) => {
     var concertId=req.params.id_concert;
 
 var user = index.getUser(req.cookies["session"]);
-if(typeof(user)=='undefined')
+if(user==undefined)
      res.redirect("/auth/login")
 else
     {
         console.log(req.params);
         var nonBuyCard = await ConcertsService.findOneNonBuyCard(concertId);
         nonBuyCard=nonBuyCard[0].cards[0];
-        if(typeof(nonBuyCard)!='undefined')
+        if(nonBuyCard!=undefined)
         {
             nonBuyCard.is_reserved = true;
             nonBuyCard.is_sold = true;
             nonBuyCard.id_user_bought = user.id_users;
             nonBuyCard.id_user_reserved = user.id_users;
             nonBuyCard =await CardService.buyCard(nonBuyCard);
-            res.redirect("/concerts");
+            res.redirect("/concerts/usersCards");
         }
         else
         {
@@ -62,7 +61,7 @@ else
 exports.buyReservedCard = async(req,res) => {
     var idCard = req.params.id_card;
     var user = index.getUser(req.cookies["session"]);
-    if(typeof(user)=='undefined')
+    if(user==undefined)
     {
         res.redirect("/auth/login");
     }
@@ -87,19 +86,21 @@ exports.buyReservedCard = async(req,res) => {
 exports.findCardsOfUser = async(req,res) =>
 {
     var user = index.getUser(req.cookies["session"]);
-    if(typeof(user)=='undefined')
+    if(user==undefined)
     {
         res.redirect("/auth/login");
     }
     else{
-        var cards = await CardService.findAllCardsOfUser(user.id_users);
-        if(cards.length>0)
-        {
-            res.render("cart/cart",{layout:"layouts/main",data:{"data":cards,"user":user}});
-        }
-        else
-        {
-            res.render("cart/cart",{layout:"layouts/main",data:{"data":cards,"user":user}});
-        }
+        if(user.role!="admin"){
+            var cards = await CardService.findAllCardsOfUser(user.id_users);
+            if(cards.length>0)
+            {
+                res.render("cart/cart",{layout:"layouts/main",data:{"data":cards,"user":user}});
+            }
+            else
+            {
+                res.render("cart/cart",{layout:"layouts/main",data:{"data":cards,"user":user}});
+            }
+        }else{res.redirect("/concerts")}
     }
 }
